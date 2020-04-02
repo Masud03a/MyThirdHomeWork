a5aeb90b160a9b3870bcc008ac0f9ad0d0576d0e
Masud03a <62532380+Masud03a@users.noreply.github.com>
2020-04-02 16:14:26 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace MySecondConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //H\W
+            
+            //1
+            /*int a = 2, b = 3;
+            if(a==b){
+                a=0;
+                b=0;
+            }
+            else{
+                if(a>b) b=a;
+                else a=b;
+            }
+
+            //2
+            System.Console.WriteLine($"a = {a}, b = {b}");
+            int operand1 = 5, operand2 = 3;
+            System.Console.WriteLine($"what i have to do with this values{operand1} and {operand2}?");
+            System.Console.WriteLine($"please choose some operation( /; *; %; -; +;)");
+            string sign = Console.ReadLine();
+            switch (sign){
+                case "/": if (operand2 == 0) System.Console.WriteLine("ERROR! This operation is wrong!");
+                else System.Console.WriteLine($"The result of this operation is {operand1/operand2}"); break;
+                case "*": System.Console.WriteLine($"The result of this operation is {operand1*operand2}"); break;
+                case "%": if (operand2 == 0) System.Console.WriteLine("ERROR! This operation is wrong!");
+                else System.Console.WriteLine($"The result of this operation is {operand1/operand2}"); break;
+                case "-": System.Console.WriteLine($"The result of this operation is {operand1-operand2}"); break;
+                case "+": System.Console.WriteLine($"The result of this operation is {operand1+operand2}"); break;
+            }*/
+        }
+    }
+}    
