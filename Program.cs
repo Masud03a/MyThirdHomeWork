0023f5ff02bd27f2777ee627f44e21f2a266230e
unknown <amirkhonov.masud90.gmail.com>
2020-04-02 15:12:32 +0300

    happy

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace MySecondConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int x=4;
+            int y=4;
+            bool result = false;
+            if(x>=y) result = true;
+            else result = false;
+            if(x==y){
+                System.Console.WriteLine($"{x}=={y}");
+                result = true;
+            }
+            else{
+                System.Console.WriteLine($"{x}=={y}");
+                result = false;
+            }
+            else if(x>y){
+                System.Console.WriteLine($"{x}=={y}");
+                result = true;
+            }
+            else{
+                System.Console.WriteLine($"{x}=={y}");
+                result = false;
+            }
+            System.Console.WriteLine(result);
+
+            int input = 8;
+            switch(input);
+            {
+                case 1:
+                    System.Console.WriteLine("One");
+                    break;
+                case 2:
+                    System.Console.WriteLine("Two");
+                    break;
+                default:
+                    System.Console.WriteLine($"There are no rules for {input}");
+                    break;
+            }*/
+
+            //H\W
+            
+            //1
+            /*int a = 2, b = 3;
+            if(a==b){
+                a=0;
+                b=0;
+            }
+            else{
+                if(a>b) b=a;
+                else a=b;
+            }
+
+            //2
+            System.Console.WriteLine($"a = {a}, b = {b}");
+            int operand1 = 5, operand2 = 3;
+            System.Console.WriteLine($"what i have to do with this values{operand1} and {operand2}?");
+            System.Console.WriteLine($"please choose some operation( /; *; %; -; +;)");
+            string sign = Console.ReadLine();
+            switch (sign){
+                case "/": if (operand2 == 0) System.Console.WriteLine("ERROR! This operation is wrong!");
+                else System.Console.WriteLine($"The result of this operation is {operand1/operand2}"); break;
+                case "*": System.Console.WriteLine($"The result of this operation is {operand1*operand2}"); break;
+                case "%": if (operand2 == 0) System.Console.WriteLine("ERROR! This operation is wrong!");
+                else System.Console.WriteLine($"The result of this operation is {operand1/operand2}"); break;
+                case "-": System.Console.WriteLine($"The result of this operation is {operand1-operand2}"); break;
+                case "+": System.Console.WriteLine($"The result of this operation is {operand1+operand2}"); break;
+            }*/
+        }        
+    }
+}
